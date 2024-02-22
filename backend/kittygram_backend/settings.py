2c81024dff10e2e787e587efaa431e3df34b6858
ruki-qq <ruki@duck.com>
2024-02-22 21:58:30 +0300

    Fix settings

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -10,7 +10,7 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', get_random_secret_key())
 
 DEBUG = os.getenv('DEBUG', 'False').lower() == 'true'
 
-ALLOWED_HOSTS = json.loads(os.environ['ALLOWED_HOSTS']) or ["127.0.0.1", "localhost"]
+ALLOWED_HOSTS = json.loads(os.getenv('ALLOWED_HOSTS', '["127.0.0.1", "localhost"]'))
 
 INSTALLED_APPS = [
     'django.contrib.admin',
